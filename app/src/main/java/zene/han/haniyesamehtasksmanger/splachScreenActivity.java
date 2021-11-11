6d92c75363b7008a435df808872a3ee3dd85431d
JamalSamniya <jamal.samniya@gmail.com>
2021-11-11 09:41:14 +0200

    last

--- app/src/main/java/zene/han/haniyesamehtasksmanger/splachScreenActivity.java
+++ app/src/main/java/zene/han/haniyesamehtasksmanger/splachScreenActivity.java
@@ -2,13 +2,34 @@ package zene.han.haniyesamehtasksmanger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class splachScreenActivity extends AppCompatActivity {
+public class splachScreenActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splach_screen);
+
+        Thread th=new Thread()
+        {
+            @Override
+            public void run()
+            {
+                //هنا المقطع الذي يعمل بالتزامن مع مقاطع اخرى
+                int mss = 3 * 1000;//millisecond
+                try {
+                    sleep(mss);
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        //Thread 4
+        th.start();
     }
 }
